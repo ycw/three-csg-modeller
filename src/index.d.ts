2af76d0a86192ddcba8b3bb54449dc8b1143f058
ycw <ycw7788@gmail.com>
2020-10-09 05:13:55 +0800

    added doccom

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,12 +3,22 @@ import Model from "./Model"
 
 declare export default class Modeller {
 
+    /**
+     * Construct a `Modeller`. Please inject the three.js library like the following:
+     * 
+     * ```js
+     * import * as THREE from "three"
+     * const modeller = new Modeller(THREE);
+     * ``` 
+     * 
+     * @param THREE - The three.js lib.
+     */
     constructor(THREE: THREE);
 
     /**
-     * Construct a model from input mesh. This mesh must use `BufferGeometry` which
-     * must contains `attributes.position` and optionally includes attributes of
-     * `normal`, `uv` and `color`.
+     * Construct a model from input mesh. The mesh must hold a `BufferGeometry` which
+     * must contain `attributes.position` and optionally include attributes `normal`, 
+     * `uv` and `color`.
      * @param mesh - The mesh to model.
      */
     model(mesh: THREE.Mesh): Model;
