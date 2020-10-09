8590ba39cf42f74b72f1ccdfd86ced7db86590a0
ycw <ycw7788@gmail.com>
2020-10-09 20:03:12 +0800

    removed comment

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,6 @@ import { terser } from "rollup-plugin-terser";
 export default {
     input: "src/index.js",
     output: [
-        // { file: "lib.js", format: "cjs" },
-        // { file: "lib.min.js", format: "cjs", plugins: [terser()] },
-        // { file: "lib.esm.js", format: "esm", plugins: [terser()] },
         { 
             file: "dist/lib.esm.js", 
             format: "esm", 
